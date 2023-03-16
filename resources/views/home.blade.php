3b292ca0521f6b50ae7aff2a47c0083d89903d8b
sarasi <sarasi.wc@gmail.com>
2023-03-16 18:52:07 +0530

    add side navigation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+
+<div class="grid-container-element w-100">
+    <div class="shadow">
+        <ul id="side-navigation">
+            <li><a class="active" href="#home">Home</a></li>
+            <li><a href="#">Employees</a></li>
+            <li><a href="#">Salaries</a></li>
+            <li><a href="#">Titles</a></li>
+        </ul>
+    </div>
+    <div class="shadow-none">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">{{ __('Dashboard') }}</div>
+
+                        <div class="card-body">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+
+                            {{ __('You are logged in!') }}
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    </div>
                 </div>
             </div>
         </div>
