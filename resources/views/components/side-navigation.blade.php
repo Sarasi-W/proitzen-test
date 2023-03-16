143a570feb3138d0bd8d3c27ffee612d2cdf8560
sarasi <sarasi.wc@gmail.com>
2023-03-17 04:13:29 +0530

    improve UI

--- resources/views/components/side-navigation.blade.php
+++ resources/views/components/side-navigation.blade.php
@@ -1,8 +1,37 @@
 <div class="shadow">
     <ul id="side-navigation">
-        <li><a class="active" href="{{ route('home') }}">Home</a></li>
-        <li><a href="{{ route('employees.index') }}">Employees</a></li>
-        <li><a href="#">Salaries</a></li>
-        <li><a href="#">Titles</a></li>
+        <li>
+            <a 
+                class="{{ Route::currentRouteName() == 'home' ? 'active' : '' }}" 
+                href="{{ route('home') }}"
+            >
+                Home
+            </a>
+        </li>
+        <li>
+            <a 
+                class="{{ in_array(Route::currentRouteName(), 
+                            ['employees.index', 'employees.create', 'employees.show', 'employees.edit']
+                        ) ? 'active' : '' }}"
+                href="{{ route('employees.index') }}">
+                Employees
+            </a>
+        </li>
+        <li>
+            <a 
+                class="{{ Route::currentRouteName() == 'salaries.index' ? 'active' : '' }}"
+                href="#"
+            >
+                Salaries
+            </a>
+        </li>
+        <li>
+            <a 
+                class="{{ Route::currentRouteName() == 'titles.index' ? 'active' : '' }}"
+                href="#"
+            >
+                Titles
+            </a>
+        </li>
     </ul>
 </div>
